3d6de538e612831c77e4467f33b22e940ee6f6e2
Robin Gustafsson <robingustafsson93@gmail.com>
2014-02-07 16:58:04 +0100

    Andy added Dice

---
+++ Projektarbete/Projektarbete/Choice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete
+{
+    class Choice
+    {
+        string description;
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+        }
+
+        public Choice(string description)
+        {
+            this.description = description;
+
+        }
+
+        int eventID;
+        public int EventID
+        {
+            get
+              {
+                  return eventID;
+              }
+        }
+
+        public Choice(int eventID)
+        {
+            this.eventID = eventID;
+        }
+    }
+}
---
+++ Projektarbete/Projektarbete/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete
+{
+    class Dice
+    {
+        int sides;
+
+        public Dice(int sides)
+        {
+            this.sides = sides; 
+        }
+
+        public int Roll() 
+        {
+            Random random = new Random();
+            int sideRolls = random.Next(sides) + 1;
+            return sideRolls;
+        }
+    }
+}
